6afefb0628f4f849b6270eaac532e344951e9307
efetigrel <efetigrel@gmail.com>
2023-12-18 16:59:25 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:personal_finance_management_application/screens/home_screen.dart';
 import 'package:personal_finance_management_application/theme.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'My Flutter App',
-      theme: AppTheme.darkTheme(), // Burada özel dark tema kullanılıyor
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('My App'),
-      ),
-      body: const Center(
-        child: Text('Merhaba, dünya!'),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Personal Finance Management Application',
+      theme: AppTheme.darkTheme(),
+      home: HomeScreen(),
     );
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,1 +1,18 @@
+import 'package:flutter/material.dart';
 
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('App'),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text('Hello'),
+      ),
+    );
+  }
+}
